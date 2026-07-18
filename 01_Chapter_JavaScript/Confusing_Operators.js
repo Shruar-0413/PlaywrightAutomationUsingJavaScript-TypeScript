09f8fc27eae653ddf28863ff965d0c20c056fb41
Shruthi V <shru.ar.1813@gmail.com>
2026-07-18 21:12:09 +0530

    Import learning materials from Playwright chapters

---
+++ 01_Chapter_JavaScript/Confusing_Operators.js
@@ -0,0 +1,8 @@
+console.log(Number(null) >= 0); // true  -> 0 >= 0
+console.log(Number(null) > 0);  // false -> 0 > 0
+
+console.log(null == undefined); // true
+console.log(null == false);     // false
+console.log(null == '');        // false
+console.log(null == 0);         // false
+
